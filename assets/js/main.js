76de84d182b29bdb9a775a4994ba423f5eaa9333
Michael Daly <mjdaly95@gmail.com>
2016-06-16 19:10:45 -0700

    (init)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+// I hope you like spaghetti
+
 var App = {};
+
+App.mouse = new THREE.Vector2(0, 0);
+App.selectedNode = [];
 
 App.scene = new THREE.Scene();
 App.camera = new THREE.PerspectiveCamera(75, 
@@ -11,6 +16,9 @@ App.controls = new THREE.OrbitControls(App.camera);
 // App.controls.addEventListener('change', App.render);
 
 document.body.appendChild(App.renderer.domElement);
+
+App.canvas = App.renderer.domElement;
+App.context = App.canvas.getContext('2d');
 
 App.camera.position.z = 4;
 
@@ -27,27 +35,6 @@ var resizeCanvas = (e) => {
 
 // Main App methods //
 
-App.createNode = (color = 'skyblue', sprite = 'node.png', name) => {
-  let map = new THREE.TextureLoader().load('assets/img/' + sprite);
-
-  let material = new THREE.SpriteMaterial({ 
-    map, 
-    color,
-    fog: true
-  });
-
-  let node = new THREE.Sprite(material);
-
-  node.name = name;
-  node.isNode = true;
-  node.edges = [];
-  node.scale.set(0.25, 0.25, 0.25);
-
-  App.scene.add(node);
-
-  return node;
-};
-
 App.createPlane = (data = {}) => {
   let geometry = new THREE.PlaneGeometry(
     data.geometry.width, 
@@ -62,31 +49,7 @@ App.createPlane = (data = {}) => {
   return plane;
 };
 
-App.linkNodes = (node1, node2) => {
-  let material = new THREE.LineBasicMaterial({ 
-    color: 0xAAAAAA,
-    linewidth: 1,
-    fog: true
-  });
 
-  let geometry = new THREE.Geometry();
-
-  let pos1 = node1.position;
-  let pos2 = node2.position;
-
-  geometry.vertices.push(
-    new THREE.Vector3(pos1.x, pos1.y, pos1.z),
-    new THREE.Vector3(pos2.x, pos2.y, pos2.z)
-  );
-  let line = new THREE.Line(geometry, material);
-
-  line.nodes = [node1, node2];
-
-  node1.edges.push(line);
-  node2.edges.push(line);
-
-  App.scene.add(line);
-};
 
 // App.scene.add(
 //   App.groundPlane = App.createPlane({
@@ -106,36 +69,11 @@ App.linkNodes = (node1, node2) => {
 
 App.render = () => {
   requestAnimationFrame(App.render);
+
   // App.controls.update();
-  App.handleMouseOver();
+  RC.handleMouseOver();
   App.renderer.render(App.scene, App.camera);
 };
-
-var main = App.createNode({
-  material: {
-    color: 'white'
-  },
-
-  node: {
-    name: 'Fred'
-  }
-});
-
-var a = App.createNode();
-var b = App.createNode();
-var c = App.createNode();
-
-
-a.position.z = 2;
-b.position.z = 1;
-b.position.x = -2;
-c.position.z = -1;
-c.position.x = 2;
-c.position.y = 2;
-
-App.linkNodes(a, main);
-App.linkNodes(b, main);
-App.linkNodes(c, main);
 
 App.init = () => { 
   App.render();
--- assets/js/raycasting.js
+++ assets/js/raycasting.js
@@ -1,22 +1,31 @@
-App.raycaster = new THREE.Raycaster();
-App.mouse = new THREE.Vector2(0, 0);
-App.selectedNode = [];
+var RC = {};
 
-App.onMouseMove = (event) => {
-  App.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  App.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;   
+RC.raycasters = {
+  hover: new THREE.Raycaster(),
+  click: new THREE.Raycaster()
 };
 
-App.handleMouseOver = () => {
+App.onMouseMove = (event) => {
+  App.mouse.isMoving = true;
+  App.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  App.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;   
+  App.mouse.isMoving = false;
+};
+
+App.onMouseDown = (event) => {
+  RC.handleMouseDown(event);
+};
+
+RC.handleMouseOver = () => {
   if (App.selectedNode.length > 0) {
     App.selectedNode[0].material.color.set(App.selectedNode[1]);
   }
 
   App.selectedNode = [];
 
-  App.raycaster.setFromCamera(App.mouse, App.camera);
+  RC.raycasters.hover.setFromCamera(App.mouse, App.camera);
 
-  let intersects = App.raycaster.intersectObjects(App.scene.children);
+  let intersects = RC.raycasters.hover.intersectObjects(App.scene.children);
   let length = intersects.length;
 
   for (let i = 0; i < length; i++) {
@@ -24,14 +33,72 @@ App.handleMouseOver = () => {
     if (intersects.length > 0) {
       if (intersects[i].object.isNode) {
 
-        App.selectedNode[0] = intersects[0].object;
+        App.selectedNode[0] = intersects[i].object;
         App.selectedNode[1] = App.selectedNode[0].material.color.getHex();
 
-        intersects[0].object.material.color.setRGB(1, 0, 0);
+        intersects[i].object.material.color.setRGB(1, 0, 0);
         break;
       }
     }
   }
 };
 
+RC.handleMouseDown = (event) => {
+  let node = App.selectedNode[0];
+
+  if (!node) { return; }
+
+  let number = Math.round(Math.random() * 6);
+
+  let nx = node.position.x;
+  let ny = node.position.y;
+  let nz = node.position.z;
+
+  let prevNode = null;
+
+  for (let i = 0; i < number; i++) {
+    let x = (Math.random() * 3);
+    let y = (Math.random() * 3);
+    let z = (Math.random() * 3);
+
+    if (Math.round(Math.random()) === 1) {
+      x = -x;
+    }
+
+    if (Math.round(Math.random()) === 1) {
+      y = -y;
+    }
+
+    if (Math.round(Math.random()) === 1) {
+      z = -z;
+    }
+
+    let newNode = Nodes.createNode([nx + x, ny + y, nz + z]);
+
+    if (Math.round(Math.random() * 2) === 1) {
+      let randomIndex = Math.round(Math.random() * (Nodes.collection.length - 1));
+      let connect = Nodes.collection[randomIndex];
+
+      if (connect !== newNode) {
+        Nodes.connectTwoNodes(connect, newNode);
+      }
+    }
+
+    Nodes.connectTwoNodes(newNode, node);
+  }
+
+  // let scale = 0;
+  // if (event.button === 0) {
+  //   scale = 0.25;
+  // } else if (event.button === 2) {
+  //   scale = -0.25;
+  // }
+
+  // if (App.selectedNode.length > 0 && !App.mouse.isMoving) {
+  //   let node = App.selectedNode[0];
+  //   node.scale.set(node.scale.x + scale, node.scale.y + scale, node.scale.z + scale);
+  // }
+}; 
+
 window.addEventListener('mousemove', App.onMouseMove, false);
+window.addEventListener('mousedown', App.onMouseDown, false);
