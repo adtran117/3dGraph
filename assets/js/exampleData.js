816ddb7179fb5a49c611b0115aca2a950cce1e4e
Michael Daly <mjdaly95@gmail.com>
2016-06-18 18:05:30 -0700

    Added example data

---
+++ assets/js/exampleData.js
@@ -0,0 +1,80 @@
+const exampleUserData = {
+  0: {
+    id: 0,
+    name: 'Fred',
+    repos: [0, 2, 3],
+    type: 'user'
+  },
+
+  1: {
+    id: 1,
+    name: 'Allen',
+    repos: [2, 5],
+    type: 'user'
+  },
+
+  4: {
+    id: 4,
+    name: 'Mike',
+    repos: [5],
+    type: 'user'
+  },
+
+  10: {
+    id: 10,
+    name: 'Karen',
+    repos: [1, 4],
+    type: 'user'
+  },
+
+  7: {
+    id: 7,
+    name: 'Steve',
+    repos: [3],
+    type: 'user'
+  }
+};
+
+const exampleRepoData = {
+  0: {
+    id: 0,
+    name: 'Test0',
+    contributers: [0],
+    type: 'repo'
+  },
+
+  1: {
+    id: 1,
+    name: 'Test1',
+    contributers: [10],
+    type: 'repo'
+  },
+
+  2: {
+    id: 2,
+    name: 'Test2', 
+    contributers: [0, 1],
+    type: 'repo'
+  },
+
+  3: {
+    id: 3,
+    name: 'Test3',
+    contributers: [0, 7],
+    type: 'repo'
+  },
+
+  4: {
+    id: 4,
+    name: 'Test4',
+    contributers: [10],
+    type: 'repo'
+  },
+
+  5: {
+    id: 5,
+    name: 'Test5',
+    contributers: [1, 4],
+    type: 'repo'
+  }
+};
