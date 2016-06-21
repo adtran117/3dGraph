fd28f4a43909cb8c319519b83bc2b9d3d305f917
Michael Daly <mjdaly95@gmail.com>
2016-06-20 19:55:24 -0700

    Minor changes; minor additions to work with major changes in other files

--- assets/js/raycasting.js
+++ assets/js/raycasting.js
@@ -1,44 +1,52 @@
-var RC = {};
+const Ray = {};
 
-RC.raycasters = {
+Ray.casters = {
   hover: new THREE.Raycaster(),
   click: new THREE.Raycaster(),
   camera: new THREE.Raycaster(),
 };
 
-RC.handleMouseOver = () => {
+Ray.handleMouseOver = () => {
   if (App.selectedNode.length > 0) {
     App.selectedNode[0].material.color.set(App.selectedNode[1]);
   }
 
   App.selectedNode = [];
 
-  RC.raycasters.hover.setFromCamera(App.mouse, App.camera);
+  Ray.casters.hover.setFromCamera(App.mouse, App.camera);
 
-  let intersects = RC.raycasters.hover.intersectObjects(App.scene.children);
+  let intersects = Ray.casters.hover.intersectObjects(App.scene.children);
   let length = intersects.length;
 
   for (let i = 0; i < length; i++) {
     if (intersects.length > 0) {
-      if (intersects[i].object.isNode) {
+      if (typeof intersects[i].object.model !== 'undefined' && 
+        intersects[i].object.model.isNode) {
 
-        App.selectedNode[0] = intersects[i].object;
-        App.selectedNode[1] = App.selectedNode[0].material.color.getHex();
-
-        intersects[i].object.material.color.setRGB(1, 0, 0);
+        intersects[i].object.model.onMouseOver();
         break;
       }
     }
   }
 };
 
-RC.handleMouseDown = (event) => {
+Ray.handleMouseDown = (event) => {
   let node = App.selectedNode[0];
 
-  if (!node) { return; }
+  if (typeof node === 'undefined') { return; }
 
-  Nodes.onClick(node);
+  let controls = App.controls;
+  let obj = controls.object;
+  let target = controls.target;
+
+  let x = Math.abs(target.x - obj.position.x);
+  let y = Math.abs(target.y - obj.position.y);
+  let z = Math.abs(target.z - obj.position.z);
+
+  let dist = new THREE.Vector3(x, y, z).length();
+
+  App.controls.minDistance = dist;
+  App.controls.maxDistance = dist;
+
+  node.view.onClick();
 }; 
-
-window.addEventListener('mousemove', App.onMouseMove);
-window.addEventListener('click', App.onMouseDown);
