230f52bba82ddb7b83b02dba7e74da786fb3bef4
Michael Daly <mjdaly95@gmail.com>
2016-06-20 19:49:33 -0700

    Cleaned up code; added moving camera to nodes

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,35 +1,59 @@
-var Controls = {
-  isMouseMoving: false
+const Controls = {};
+
+Controls.init = () => {
+  Controls.destination = App.controls.target;
+  Controls.targetObj;
+  Controls.minDistance = App.controls.minDistance;
+  Controls.maxDistance = App.controls.maxDistance;
 };
 
-App.onMouseMove = (event) => {
+Controls.onMouseMove = (event) => {
   App.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   App.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  RC.raycasters.camera.setFromCamera(App.mouse, App.camera);
-  // console.log(RC.raycasters.camera.ray);
-
-  // Controls.update();
+  Ray.casters.camera.setFromCamera(App.mouse, App.camera);
 };
 
-App.onMouseDown = (event) => {
-  RC.handleMouseDown(event);
+Controls.onMouseDown = (event) => {
+  Ray.handleMouseDown(event);
 };
 
 Controls.update = () => {
-  let dir = RC.raycasters.camera.ray.direction;
-  let newDir = new THREE.Vector3(dir.x * 10, dir.y * 10, dir.z * 10);
-  // let rot = App.camera.rotation;
-    // console.log(dir);
+  let dest = Controls.destination;
+  let pos = App.controls.target;
 
-  // console.log(newDir);
-  App.camera.lookAt(newDir);
+  if (dest.x !== pos.x || dest.y !== pos.y || dest.z !== pos.z) {
 
-  // if (rot.x !== dir.x) {
-  //   let diff = Math.sign(rot.x - dir.y) / 20;
+    // Get difference between distance and camera's focus point
+    let diffx = dest.x - pos.x;
+    let diffy = dest.y - pos.y;
+    let diffz = dest.z - pos.z;
 
-  //   rot.x += diff;
-  // }
-  // App.camera.rotation.x += (App.mouse.y / 20);
-  // App.camera.rotation.y += -(App.mouse.x / 20);
+    let mul = 0.05;
+
+    if (Math.abs(diffx) >= mul) {
+      App.controls.target.x += (Math.sign(diffx) * mul);
+    } else {
+      // To prevent infinite jittering
+      App.controls.target.x = dest.x;
+    }
+
+    if (Math.abs(diffy) >= mul) {
+      App.controls.target.y += (Math.sign(diffy) * mul);
+    } else {
+      App.controls.target.y = dest.y;
+    }
+
+    if (Math.abs(diffz) >= mul) {
+      App.controls.target.z += (Math.sign(diffz) * mul);
+    } else {
+      App.controls.target.z = dest.z;
+    }
+  } else {
+    App.controls.minDistance = Controls.minDistance;
+    App.controls.maxDistance = Controls.maxDistance;
+  }
 };
+
+window.addEventListener('mousemove', Controls.onMouseMove);
+window.addEventListener('click', Controls.onMouseDown);
