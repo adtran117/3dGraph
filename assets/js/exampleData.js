d8952149c24f8945374488bf55d8d53472214256
Michael Daly <mjdaly95@gmail.com>
2016-06-20 19:50:59 -0700

    Added more connections

--- assets/js/exampleData.js
+++ assets/js/exampleData.js
@@ -2,35 +2,35 @@ const exampleUserData = {
   0: {
     id: 0,
     name: 'Fred',
-    repos: [0, 2, 3],
+    connectedTo: [0, 2, 3, 5],
     type: 'user'
   },
 
   1: {
     id: 1,
     name: 'Allen',
-    repos: [2, 5],
+    connectedTo: [2, 5],
     type: 'user'
   },
 
   4: {
     id: 4,
     name: 'Mike',
-    repos: [5],
+    connectedTo: [5],
     type: 'user'
   },
 
   10: {
     id: 10,
     name: 'Karen',
-    repos: [1, 4],
+    connectedTo: [1, 4],
     type: 'user'
   },
 
   7: {
     id: 7,
     name: 'Steve',
-    repos: [3],
+    connectedTo: [3, 1],
     type: 'user'
   }
 };
@@ -39,42 +39,42 @@ const exampleRepoData = {
   0: {
     id: 0,
     name: 'Test0',
-    contributers: [0],
+    connectedTo: [0],
     type: 'repo'
   },
 
   1: {
     id: 1,
     name: 'Test1',
-    contributers: [10],
+    connectedTo: [10],
     type: 'repo'
   },
 
   2: {
     id: 2,
     name: 'Test2', 
-    contributers: [0, 1],
+    connectedTo: [0, 1],
     type: 'repo'
   },
 
   3: {
     id: 3,
     name: 'Test3',
-    contributers: [0, 7],
+    connectedTo: [0, 7, 1],
     type: 'repo'
   },
 
   4: {
     id: 4,
     name: 'Test4',
-    contributers: [10],
+    connectedTo: [10],
     type: 'repo'
   },
 
   5: {
     id: 5,
     name: 'Test5',
-    contributers: [1, 4],
+    connectedTo: [1, 4],
     type: 'repo'
   }
 };
